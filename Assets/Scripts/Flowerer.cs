367e01bdb4898c6938666a01f46198c76c4cba5f
Kerstin Äkke <kerstinakke@hot.ee>
2018-12-26 00:39:03 +0200

    some simple petals

---
+++ Assets/Scripts/Flowerer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flowerer : MonoBehaviour
+{
+    public Color color;
+    public int petal_num=3;
+    [SerializeField]
+    private GameObject petal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float angle = 360 / petal_num;
+        float width = 3f / petal_num;
+        List<GameObject> petals = new List<GameObject>();
+        for (int i = 0; i < petal_num; i++) {
+            GameObject p = Instantiate(petal,transform);
+            Petal_props properties = p.GetComponent<Petal_props>();
+            properties.petal_color = color;
+            properties.angle = new Vector3(0, 0, angle*i);
+            properties.width = width;
+            petals.Add(p);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Petal_props.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Petal_props : MonoBehaviour
+{   
+    public Color petal_color;
+    public Vector3 angle;
+    public float width;
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        mat.color = petal_color;
+        transform.localScale = new Vector3(width,1,0.05f);
+        transform.localRotation = Quaternion.Euler(angle);
+        transform.position -= transform.up*0.5f;
+    }
+
+}
